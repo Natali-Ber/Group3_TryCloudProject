021a12b2d111a4b6555fa076f6a8988c68770a95
Tariq Ejaz <mtariq_ejaz@yahoo,com>
2022-04-03 23:38:08 -0400

    US9_LoginPAge

---
+++ src/test/java/com/cydeo/pages/LoginPage.java
@@ -0,0 +1,27 @@
+package com.cydeo.pages;
+
+import com.cydeo.utilities.ConfigurationReader;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends  BasePage {
+
+    @FindBy(xpath = "//input[@name='user']")
+    public WebElement inputUsername;
+
+    @FindBy(id = "password")
+    public WebElement inputPassword;
+
+    @FindBy(id="submit-form")
+    public WebElement loginButton;
+
+    @FindBy(css = "p[class='warning wrongPasswordMsg']")
+    public WebElement errorMsg;
+
+    public void loginWithConfig(){
+        inputUsername.sendKeys(ConfigurationReader.getProperty("username1"));
+        inputPassword.sendKeys(ConfigurationReader.getProperty("password"));
+        loginButton.click();
+    }
+
+}
